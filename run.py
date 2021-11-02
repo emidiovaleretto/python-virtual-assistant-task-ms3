f640aa4321cec3415bd77364a40d46394ead49cb
Emidio Valereto <emidio.valereto@gmail.com>
2021-11-02 23:10:18 +0000

    Create a function to remove a task from the list.

--- run.py
+++ run.py
@@ -58,24 +58,54 @@ def print_menu():
     )
 
 
-def add_new_task(task):
+def add_new_task():
     """
     Adds a new task to the list.
     """
+    chatboot_message("\nWhat task would you like to add?\n")
+    task = input("\n>> ")
     task_list.append(task)
     chatboot_message(f"\nGreat! Let's add [{task.upper()}] to your list.\n")
     chatboot_message("Adding task...")
     sleep(1)
-    return chatboot_message(f"\nTask added!\n")
+    chatboot_message(f"\nTask added!\n")
 
 
 def view_all_tasks():
     """
     Prints a list of tasks.
     """
-    for i, task in enumerate(task_list):
-        i += 1
-        print(f"{i} - {task}")
+    if not task_list:
+        sleep(1)
+        chatboot_message("\nYour list is still empty.\n")
+
+    else:
+        chatboot_message(f"\nHere is your list of tasks:\n")
+
+        for i, task in enumerate(task_list):
+            i += 1
+            print(f"{i} - {task}")
+
+
+def remove_task():
+    """
+    Returns a removed task from the list.
+    """
+    if not task_list:
+        sleep(1)
+        chatboot_message("\nThere is no tasks to be removed.\n")
+
+    else:
+        chatboot_message(f"\nWhich task would you like to delete?\n")
+        view_all_tasks()
+        task_to_delete = int(input("\n>> "))
+
+        for i, task in enumerate(task_list):
+            if task_to_delete == i + 1:
+                removed_task = task_list.pop(i)
+                removed_items.append(removed_task)
+                sleep(1)
+                return chatboot_message(f"\nTask [{task}] removed!\n")
 
 
 def main():
@@ -85,7 +115,9 @@ def main():
     chosen_name = choose_name_randomly()
     greetings(chosen_name)
     username = input("\n>> ")
-    chatboot_message(f"\nRight, {username}! How may I assist you today?\n")
+    chatboot_message(
+        f"\Hi, {username}. Thank you for using our chat service.! \nHow may I assist you today?\n"
+    )
 
     while True:
         print_menu()
@@ -97,17 +129,20 @@ def main():
             user_choice = int(user_choice)
 
             if user_choice == 1:
-                chatboot_message("\nWhat task would you like to add?\n")
-                task = input("\n>> ")
-                add_new_task(task)
+                add_new_task()
 
             elif user_choice == 2:
-                chatboot_message(f"\nHere is your list of tasks:\n")
                 view_all_tasks()
+
+            elif user_choice == 3:
+                remove_task()
+
             else:
                 chatboot_message("\nOption not available yet.\n")
                 break
 
 
+removed_items = []
 task_list = []
+
 main()
