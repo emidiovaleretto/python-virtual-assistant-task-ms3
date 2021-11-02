7ebe4209cf263b9b3b163761151068340d6280ac
Emidio Valereto <emidio.valereto@gmail.com>
2021-11-02 17:14:50 +0000

    Create a function to get a random name from a list of names.

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import choice
+
+
+def choose_name_randomly():
+    """
+    Returns a random name from a list of names.
+    """
+    list_names = [
+        'John', 'Paul', 'George', 'Ringo', 'Sam', 'Ciara', 'Fiona', 'Jessica',
+        'Natalie', 'Emma'
+    ]
+    return choice(list_names)
+
+
+chosen_name = choose_name_randomly()
+print(f'Hello, {chosen_name}!')
