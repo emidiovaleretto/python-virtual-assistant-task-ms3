9dc617b15f25bffb811bc8bc34c5598833dfa7fa
Emidio Valereto <emidio.valereto@gmail.com>
2021-11-05 17:45:39 +0000

    Create a function that sends an email to the user containing the conversation log.

---
+++ send_email.py
@@ -0,0 +1,28 @@
+import smtplib
+import ssl
+from decouple import config
+
+host = config("HOST")
+port = config("PORT")
+email_address = config("EMAIL_ADDRESS")
+password = config("PASSWORD")
+
+# Create a secure SSL context
+context = ssl.create_default_context()
+
+
+def send_email(user_email_address, message):
+    """
+    This function sends an email from the email
+    address specified in the config file to the
+    email address specified in the function call.
+    The message will be the conversation log.
+    """
+
+    with smtplib.SMTP_SSL(host=host, port=port, context=context) as server:
+        server.login(email_address, password=password)
+        server.sendmail(
+            to_addrs=user_email_address,
+            from_addr=email_address,
+            msg=message,
+        )
